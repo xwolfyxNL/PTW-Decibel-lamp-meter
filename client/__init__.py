cfa1b4264843e03c33ee7f20619cbf4706417d32
xwolfyxNL <41stefan@gmail.com>
2019-01-21 15:29:44 +0100

    Moved the init filt to a new location and fixed the path

--- client/__init__.py
+++
@@ -1,4 +0,0 @@
-PTW-Decibel-lamp-meter/includes/database.py
-PTW-Decibel-lamp-meter/includes/gpio.py
-PTW-Decibel-lamp-meter/includes/imports.py
-PTW-Decibel-lamp-meter/includes/pyaudio.py
---
+++ client/includes/__init__.py
@@ -0,0 +1,4 @@
+PTW-Decibel-lamp-meter/client/includes/database.py
+PTW-Decibel-lamp-meter/client/includes/gpio.py
+PTW-Decibel-lamp-meter/client/includes/imports.py
+PTW-Decibel-lamp-meter/client/includes/pyaudio.py
