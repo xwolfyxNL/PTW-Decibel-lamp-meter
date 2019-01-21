e15f9cba63753afe6a28a431cefbbe0f96770939
xwolfyxNL <41stefan@gmail.com>
2019-01-21 16:13:39 +0100

    Added database fetch

--- client/includes/lights.py
+++ client/includes/lights.py
@@ -1,3 +1,12 @@
+# Fetch dB values from the Mysql database
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM profielen where naam = 'ingesteld'")
+data = mycursor.fetchall()
+for row in data:
+  profilename = str(row[1])
+  dbmin = int(row[2])
+  dbmax = int(row[3])
+
 # Prints the name and the values of the loaded profile
 print ("Loaded profile information:\nName: " + profilename + "\ndB min: " + str(dbmin) + "\ndB max: " + str(dbmax) + "\n")
 
@@ -10,12 +19,12 @@ while True:
 		GPIO.output(YELLOW, 0)
 		GPIO.output(RED, 0)
 		print("Level Green\ndB: " + str(result) + "\n") # Print the level green and the dB value
-    if result > dbmin and result < dbmax: # If the result is bigger then the dbmin value and smaller then the dbmax value, then turn the light to yellow
+    elif result > dbmin and result < dbmax: # If the result is bigger then the dbmin value and smaller then the dbmax value, then turn the light to yellow
 		GPIO.output(GREEN, 0)
 		GPIO.output(YELLOW, 1)
 		GPIO.output(RED, 0)
 		print("Level Yellow\ndB: " + str(result) + "\n") # Print the level yellow and the dB value
-    if result > dbmax: # If the result is bigger then the dbmax value, then turn the light to red
+    else result > dbmax: # If the result is bigger then the dbmax value, then turn the light to red
 		GPIO.output(GREEN, 0)
 		GPIO.output(YELLOW, 0)
 		GPIO.output(RED, 1)
