d702ce012652e97ba8cbcd11c8cf63dc600109fa
xwolfyxNL <41stefan@gmail.com>
2019-01-21 15:39:37 +0100

    Changed init file

--- client/includes/__init__.py
+++ client/includes/__init__.py
@@ -1,4 +1,7 @@
-PTW-Decibel-lamp-meter/client/includes/database.py
-PTW-Decibel-lamp-meter/client/includes/gpio.py
-PTW-Decibel-lamp-meter/client/includes/imports.py
-PTW-Decibel-lamp-meter/client/includes/pyaudio.py
+client/
+      __init__.py
+      includes/
+              imports.py
+              database.py
+              gpio.py
+              pyaudio.py
