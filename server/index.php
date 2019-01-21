1d7a0e37b3df1b3548cd9f2858303d786b1dc3dc
Max Stoltenborgh <max.stoltenborgh@student.hu.nl>
2019-01-21 19:42:44 +0100

    Did some more code cleanup.

--- server/index.php
+++ server/index.php
@@ -1,36 +1,35 @@
 
 <?php
+    $database = "lampapplication"
+    $table = "profiles"
     include $_SERVER['DOCUMENT_ROOT'].'/modules/database.php'
-?>
-
-<?php
-//insert into database and reload page
-  if(isset($_POST['save']))
-{
-   	$sql0 = "INSERT INTO profiles (name, minimum, maximum)
-   	VALUES ('".$_POST["profilename"]."','".$_POST["minimum"]."','".$_POST["maximum"]."')";
-   	$result = mysqli_query($conn,$sql0);
-	header("Refresh:0");
-}
-//delete from database and reload page
-if(isset($_POST['delete']) && $_POST['profiles'] != 'current' && $_POST['profiles'] != 'default')
-{
+    //insert into database and reload page
+    if(isset($_POST['save']))
+    {
+      	$sql0 = "INSERT INTO profiles (name, minimum, maximum)
+      	VALUES ('".$_POST["profilename"]."','".$_POST["minimum"]."','".$_POST["maximum"]."')";
+     	$result = mysqli_query($conn,$sql0);
+	    header("Refresh:0");
+    }
+    //delete from database and reload page
+    if(isset($_POST['delete']) && $_POST['profiles'] != 'current' && $_POST['profiles'] != 'default')
+    {
 		$sql1 = "DELETE FROM profiles WHERE name = '".$_POST['profiles']."'";
 		$result = mysqli_query($conn,$sql1);
 		header("Refresh:0");
-}
-//update the current profile
-if (isset($_POST['use']))
-{
-	$sql2 = "SELECT minimum, maximum FROM profiles WHERE name = '".$_POST['profiles']."'";
-	$result0 = mysqli_query($conn, $sql2);
-	$values = mysqli_fetch_assoc($result0);
-	$min = $values['minimum'];
-	$max = $values['maximum'];
-	$sql3 = "UPDATE profiles SET minimum = ".$min.", maximum = ".$max." WHERE name = 'current'";
-	$result1 = mysqli_query($conn, $sql3);
-	header("Refresh:0");
-}
+    }
+    //update the current profile
+    if (isset($_POST['use']))
+    {
+	    $sql2 = "SELECT minimum, maximum FROM profiles WHERE name = '".$_POST['profiles']."'";
+	    $result0 = mysqli_query($conn, $sql2);
+	    $values = mysqli_fetch_assoc($result0);
+	    $min = $values['minimum'];
+	    $max = $values['maximum'];
+	    $sql3 = "UPDATE profiles SET minimum = ".$min.", maximum = ".$max." WHERE name = 'current'";
+	    $result1 = mysqli_query($conn, $sql3);
+	    header("Refresh:0");
+    }
 ?>
 
 
